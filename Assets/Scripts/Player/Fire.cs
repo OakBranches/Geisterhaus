bf4e8b5fcfdadd874b43a45748c16c2edfef96ba
gabrielpenajo <gabrielpenajo13@gmail.com>
2020-03-21 20:10:48 -0300

    Tiro ao manter tecla pressionada

--- Assets/Scripts/Player/Fire.cs
+++ Assets/Scripts/Player/Fire.cs
@@ -11,6 +11,8 @@ public class Fire : MonoBehaviour
     public GameObject projectile;
     public int numProjectiles = 5;
     public float projectileSpeed = 1f;
+    public float projectileTimer = 0.25f;
+    float lastProjectileTimer = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -29,13 +31,23 @@ public class Fire : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1") && projectilePool.Count > 0)
+        if (lastProjectileTimer > 0)
+        {
+            lastProjectileTimer -= Time.fixedDeltaTime;
+        }
+        else
+        {
+            lastProjectileTimer = projectileTimer;
+        }
+
+        if (Input.GetButton("Fire1") && projectilePool.Count > 0 && lastProjectileTimer <= 0f)
         {
             Projectiles.Projectile projectile = projectilePool.Dequeue();
             projectile.instance.SetActive(true);
             projectile.instance.transform.position = transform.position;
             projectile.rb.velocity =
                 new Vector3(projectileSpeed * transform.localScale.x, 0f, 0f);
+            lastProjectileTimer = lastProjectileTimer;
             Debug.Log("Fired!");
         }
     }
