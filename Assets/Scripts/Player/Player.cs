15be5c31ed2b39ea543aaebd79608812bc26891f
Sfrev <51010839+Sfrev@users.noreply.github.com>
2020-03-23 21:24:23 -0300

    Upstairs door management
    Quick movement through rooms

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -10,12 +10,14 @@ public class Player : MonoBehaviour
     Rigidbody2D rb;
     bool fire;
     bool facingRight = true;
+    Camera mainCamera;
 
     // Start is called before the first frame update
     void Start()
     {	
 		lifeManager = GetComponent<LifeManager>();
         rb = GetComponent<Rigidbody2D>();
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
@@ -45,5 +47,48 @@ public class Player : MonoBehaviour
         Vector3 newScale = transform.localScale;
         newScale.x = -newScale.x;
         transform.localScale = newScale;
+    }
+
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        UpstairCorridorLeftDoor(collider);
+        UpstairCorridorRightDoor(collider);
+        ChildBedroomRightDoor(collider);
+        ParentBedroomLeftDoor(collider);
+    }
+
+    private void UpstairCorridorLeftDoor(Collider2D collider)
+    {
+        if (collider.name == "Upstairs Corridor Left Door" && Input.GetKey(KeyCode.E))
+        {
+            gameObject.transform.position = new Vector2(-50f, -2f);
+            mainCamera.transform.position = new Vector3(-50f, 0f, -10f);
+        }
+    }
+
+    private void UpstairCorridorRightDoor(Collider2D collider)
+    {
+        if (collider.name == "Upstairs Corridor Right Door" && Input.GetKey(KeyCode.E))
+        {
+            gameObject.transform.position = new Vector2(50f, -2f);
+            mainCamera.transform.position = new Vector3(50f, 0f, -10f);
+        }
+    }
+
+    private void ChildBedroomRightDoor(Collider2D collider)
+    {
+        if (collider.name == "Child's Bedroom Right Door" && Input.GetKey(KeyCode.E))
+        {
+            gameObject.transform.position = new Vector2(0f, -2f);
+            mainCamera.transform.position = new Vector3(0f, 0f, -10f);
+        }
+    }
+    private void ParentBedroomLeftDoor(Collider2D collider)
+    {
+        if (collider.name == "Parent's Bedroom Left Door" && Input.GetKey(KeyCode.E))
+        {
+            gameObject.transform.position = new Vector2(0f, -2f);
+            mainCamera.transform.position = new Vector3(0f, 0f, -10f);
+        }
     }
 }
